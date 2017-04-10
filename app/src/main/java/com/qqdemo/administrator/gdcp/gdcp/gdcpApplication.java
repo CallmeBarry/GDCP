a1d447073f53cc0503ca8c05fb5006a53e583ee1
Administrator <1024849472@qq.com>
2017-04-10 09:13:06 +0800

    登录页面完成，okgo的全局部署，实现cookie的保存，点击登陆获取用户名

---
+++ app/src/main/java/com/qqdemo/administrator/gdcp/gdcp/gdcpApplication.java
@@ -0,0 +1,55 @@
+package com.qqdemo.administrator.gdcp.gdcp;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.store.MemoryCookieStore;
+import com.lzy.okgo.cookie.store.PersistentCookieStore;
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Created by Administrator on 2017/4/9.
+ */
+
+public class gdcpApplication extends Application {
+    String TAG = "1111111111111";
+//    public static OkHttpClient mOkHttpClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //腾讯的bugly初始化
+        CrashReport.initCrashReport(getApplicationContext(), "70db45d6c7", true);
+
+        OkGo.init(this);
+
+        //以下设置的所有参数是全局参数,同样的参数可以在请求的时候再设置一遍,那么对于该请求来讲,请求中的参数会覆盖全局参数
+        //好处是全局参数统一,特定请求可以特别定制参数
+
+        //以下都不是必须的，根据需要自行选择,一般来说只需要 debug,缓存相关,cookie相关的 就可以了
+        OkGo.getInstance()
+                //如果使用默认的 60秒,以下三行也不需要传
+                .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)  //全局的连接超时时间
+                .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)     //全局的读取超时时间
+                .setWriteTimeOut(OkGo.DEFAULT_MILLISECONDS)    //全局的写入超时时间
+
+                //可以全局统一设置缓存模式,默认是不使用缓存,可以不传,具体其他模式看 github 介绍 https://github.com/jeasonlzy/
+                .setCacheMode(CacheMode.IF_NONE_CACHE_REQUEST)
+
+                //可以全局统一设置缓存时间,默认永不过期,具体使用方法看 github 介绍
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)
+
+                //可以全局统一设置超时重连次数,默认为三次,那么最差的情况会请求4次(一次原始请求,三次重连请求),不需要可以设置为0
+                .setRetryCount(3)
+
+
+                //如果不想让框架管理cookie（或者叫session的保持）,以下不需要
+                .setCookieStore(new MemoryCookieStore())            //cookie使用内存缓存（app退出后，cookie消失）
+                .setCookieStore(new PersistentCookieStore());        //cookie持久化存储，如果cookie不过期，则一直有效
+
+
+    }
+}
--- app/src/main/java/com/qqdemo/administrator/gdcp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/qqdemo/administrator/gdcp/ui/activity/LoginActivity.java
@@ -1,13 +1,107 @@
 package com.qqdemo.administrator.gdcp.ui.activity;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.BitmapCallback;
+import com.lzy.okgo.callback.StringCallback;
 import com.qqdemo.administrator.gdcp.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2017/4/9.
  */
 public class LoginActivity extends BaseActivity {
+    private static final String TAG = "LoginActivity";
+    @BindView(R.id.ed_id)
+    EditText mEdId;
+    @BindView(R.id.ed_pwd)
+    EditText mEdPwd;
+    @BindView(R.id.iv_code)
+    ImageView mIvCode;
+    @BindView(R.id.btn_changecode)
+    Button mBtnChangecode;
+    @BindView(R.id.ed_code)
+    EditText mEdCode;
+    @BindView(R.id.btn_login)
+    Button mBtnLogin;
+
+    String codeurl = "http://jw2012.gdcp.cn/CheckCode.aspx";
+
     @Override
     public int getLayoutResId() {
         return R.layout.activity_login;
     }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        initCodeImg();
+
+
+    }
+
+    private void initCodeImg() {
+        OkGo.get(codeurl)
+                .tag(this)
+                .execute(new BitmapCallback() {
+                    @Override
+                    public void onSuccess(final Bitmap bitmap, Call call, Response response) {
+                                mIvCode.setImageBitmap(bitmap);
+                    }
+                });
+    }
+
+    @OnClick({R.id.btn_changecode, R.id.btn_login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_changecode:
+                initCodeImg();
+                break;
+            case R.id.btn_login:
+                login();
+                break;
+        }
+    }
+
+    private void login() {
+
+        OkGo.post("http://jw2012.gdcp.cn")
+                .params("__VIEWSTATE", "dDw3OTkxMjIwNTU7Oz72G0jnx2CVi9cEqCETKg2lgGSYBw==")
+                .params("TextBox1", "1513157221")// mEdId.getText().toString().trim()
+                .params("TextBox2", "1111111")//mEdPwd.getText().toString().trim()
+                .params("TextBox3", mEdCode.getText().toString().trim())
+                .params("RadioButtonList1", "学生")
+                .params("Button1", "")
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(String s, Call call, Response response) {
+
+                        Log.i(TAG, "onResponse: " + s.toString());
+
+                        Document doc = Jsoup.parse(s);
+                        Element xhxm = doc.getElementById("xhxm");
+                        Toast.makeText(LoginActivity.this,xhxm.text().toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+
 }
